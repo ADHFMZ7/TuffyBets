d89a43cc61f771600b1da20696df86ee75c1325b
adhfmz7 <ahmadaldasouqi559@gmail.com>
2024-02-02 21:07:22 -0800

    begin documenting user api

--- api/src/app.py
+++ api/src/app.py
@@ -10,9 +10,24 @@ def index():
 
 @app.get("/register")
 def register():
+    """
+    This endpoint is used to create a new user account.
+
+    It takes in a POST request containing the form data 
+    from the sign up page.
+
+    It sends HTTP 200 response if an account was properly
+    created and ___ on error.
+    """
     pass
 
 @app.get("/signin")
 def signin():
+    """
+    This endpoint is used to authenticate a user to 
+    gain access to their existing account.
+
+    
+    """
     pass
 
